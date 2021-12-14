f219bf9c8a8c1fc9357a6bec9defbf469cee60de
STEINMETZ THOMAS <thomas.steinmetz2@etu.unistra.fr>
2021-12-14 13:37:33 +0100

    :construction: add template

---
+++ cocktail_app/lib/core/viewmodels/home_model.dart
@@ -0,0 +1,18 @@
+import 'package:cocktail_app/core/enums/viewstate.dart';
+import 'package:cocktail_app/core/models/post.dart';
+import 'package:cocktail_app/core/services/posts_service.dart';
+import 'package:cocktail_app/locator.dart';
+
+import 'base_model.dart';
+
+class HomeModel extends BaseModel {
+  PostsService _postsService = locator<PostsService>();
+  
+  List<Post> get posts => _postsService.posts;
+
+  Future getPosts(int userId) async {
+    setState(ViewState.Busy);
+    await _postsService.getPostsForUser(userId);
+    setState(ViewState.Idle);
+  }
+}
---
+++ cocktail_app/lib/core/viewmodels/like_button_model.dart
@@ -0,0 +1,19 @@
+import 'package:cocktail_app/core/services/posts_service.dart';
+import 'package:cocktail_app/locator.dart';
+
+import 'base_model.dart';
+
+class LikeButtonModel extends BaseModel {
+  PostsService _postsService = locator<PostsService>();
+
+  int postLikes(int postId) {
+    return _postsService.posts
+        .firstWhere((post) => post.id == postId)
+        .likes;
+  }
+
+  void increaseLikes(int postId) {
+    _postsService.incrementLikes(postId);
+    notifyListeners();
+  }
+}
